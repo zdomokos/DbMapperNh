6166967b449afabc76d1c01c218670f9875ffb85
Zsolt Domokos <Zsolt.Domokos@avmltd.com>
2020-05-13 14:57:40 -0400

    1st

---
+++ DbMapperNh/Program.cs
@@ -0,0 +1,157 @@
+﻿using log4net;
+using log4net.Appender;
+using log4net.Layout;
+using log4net.Repository.Hierarchy;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+using NHibernate.Tool.hbm2ddl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratorTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+
+            var types = new List<Type>() { typeof(Color3Map) };
+            var mapper = new ModelMapper();
+            mapper.AddMappings(types);
+            var mapping = mapper.CompileMappingForAllExplicitlyAddedEntities();
+
+            var configuration = new Configuration();
+            configuration.DataBaseIntegration(db =>
+            {
+                db.ConnectionString = "host=localhost;database=sandbox;user id=postgres;password=Asztal15";
+                db.Driver<NpgsqlDriver>();
+                db.Dialect<PostgreSQLDialect>();
+                db.LogFormattedSql = true;
+                db.LogSqlInConsole = true;
+            });
+            configuration.SetNamingStrategy(DbNamingStrategy.Instance); // add before mappings!
+
+            // add xml configs: configuration.AddAssembly(Assembly.GetExecutingAssembly());
+            configuration.AddDeserializedMapping(mapping, null);
+            //SchemaMetadataUpdater.QuoteTableAndColumns(configuration); //configuration.SetProperty("hbm2ddl.keywords", "auto-quote");
+
+            var sefact = configuration.BuildSessionFactory();
+
+            using (var session = sefact.OpenSession())
+            {
+                //var colors = session.Query<Color3>().Where(r => r.Id > 0).ToList();
+
+
+                //var newCol = new Color3 { Name = "new1" };
+                //session.Save(newCol);
+            }
+
+            using (var session = sefact.OpenSession())
+            {
+                var newCol = new Color3 { Name = "new1" };
+                var ret = session.Save(newCol);
+                Console.WriteLine(ret.ToString());
+            }
+
+            using (var session = sefact.OpenStatelessSession())
+            {
+                var newCol = new Color3 { Name = "new1" };
+                var ret = session.Insert(newCol);
+                Console.WriteLine(ret.ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Color3Map : ClassMapping<Color3>
+    {
+        public Color3Map()
+        {
+            Schema("public");
+            Lazy(false);
+            //Id(x => x.Id, map => { map.Generator(Generators.Identity, p => p.Params(new { sequence = "`Color3_Id_seq`" })); map.Column("`Id`"); });
+            Id(x => x.Id, map =>
+            {
+                map.Generator(Generators.TriggerIdentity, p => p.Params(new
+                {
+                    sequence = "Color3_Id_seq"
+                }
+                ));
+            });
+            Property(x => x.Name, map => { map.NotNullable(true); });
+            Property(x => x.Description);
+        }
+    }
+
+    //public class Color3Map : ClassMapping<Color3>
+    //{
+    //    public Color3Map()
+    //    {
+    //        Table("`Color3`");
+    //        Schema("public");
+    //        Lazy(false);
+    //        //Id(x => x.Id, map => { map.Generator(Generators.Identity, p => p.Params(new { sequence = "`Color3_Id_seq`" })); map.Column("`Id`"); });
+    //        Id(x => x.Id, map => { map.Generator(Generators.TriggerIdentity, p => p.Params(new { sequence = "`Color3_Id_seq`" })); map.Column("`Id`"); });
+    //        Property(x => x.Name, map => { map.Column("`Name`"); map.NotNullable(true); });
+    //        Property(x => x.Description, map => { map.Column("`Description`"); });
+    //    }
+    //}
+
+    public class Color3
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    internal class DbNamingStrategy : INamingStrategy
+    {
+        private static readonly INamingStrategy ImprovedNamingStrategy = NHibernate.Cfg.ImprovedNamingStrategy.Instance;
+
+        private static DbNamingStrategy _namingStrategy;
+        public static INamingStrategy Instance => _namingStrategy ?? (_namingStrategy = new DbNamingStrategy());
+
+        protected DbNamingStrategy()
+        {
+        }
+
+        public string ClassToTableName(string className)
+        {
+            return $"\"{className}\"";
+        }
+
+        public string ColumnName(string columnName)
+        {
+            return $"\"{columnName}\"";
+        }
+
+        public string LogicalColumnName(string columnName, string propertyName)
+        {
+            return ImprovedNamingStrategy.LogicalColumnName(columnName, propertyName);
+        }
+
+        public string PropertyToColumnName(string propertyName)
+        {
+            return ImprovedNamingStrategy.PropertyToColumnName(propertyName);
+        }
+
+        public string PropertyToTableName(string className, string propertyName)
+        {
+            return ImprovedNamingStrategy.PropertyToTableName(className, propertyName);
+        }
+
+        public string TableName(string tableName)
+        {
+            return ImprovedNamingStrategy.TableName(tableName);
+        }
+    }
+}
---
+++ DbMapperNh/dbData.sql
@@ -0,0 +1,48 @@
+﻿-- snake case table
+drop table color2;
+
+CREATE TABLE color2 (
+    color_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    color_name VARCHAR NOT NULL
+);
+
+INSERT INTO color2 (color_id, color_name) OVERRIDING SYSTEM VALUE VALUES (2, 'White');
+INSERT INTO color2 (color_id, color_name) VALUES (NULL, 'White');
+INSERT INTO color2 (color_name) VALUES ('White3');
+INSERT INTO color2 (color_id, color_name) VALUES (3, 'White');
+INSERT INTO color2 (color_name) VALUES ('White3');
+INSERT INTO color2 (color_name) VALUES ('White3');
+
+
+SELECT * from color2_color_id_seq;
+
+update color2_color_id_seq   set last_value=10;
+
+SELECT setval('color2_color_id_seq', 21, true);
+
+
+
+
+
+-- quoted table
+drop table color3;
+
+CREATE TABLE "Color3"
+(
+    "Id"          integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY ,
+    "Name"        varchar(30) NOT NULL,
+    "Description" text
+);
+
+INSERT INTO "Color3" ("Id", "Name") OVERRIDING SYSTEM VALUE VALUES (2, 'White');
+INSERT INTO "Color3" ("Id", "Name") VALUES (NULL, 'White');
+INSERT INTO "Color3" ("Name") VALUES ('White3');
+INSERT INTO "Color3" ("Id", "Name") VALUES (3, 'White');
+INSERT INTO "Color3" ("Name") VALUES ('White3');
+INSERT INTO "Color3" ("Name") VALUES ('White3');
+
+select * from "Color3_Id_seq";
+
+nextval("Color3_Id_seq");
+
+
